d83ef0d7ccad7845f051d13e2abc7e3e56572026
Owethu927 <owethusilela@gmail.com>
2024-10-20 09:30:12 +0100

    Added a const for the random number and alphabet

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ function validateSyntax() {
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        
+        const regex = /^pet_\d{4}[a-zA-Z]+$/;
+
+        if (regex.test(input)) {
+            result = 'Valid Syntax' ;
+        } else {
+            result = 'Invalid Syntax'
+        }
+
+        
 
             document.getElementById('result').innerText = result;
 }
